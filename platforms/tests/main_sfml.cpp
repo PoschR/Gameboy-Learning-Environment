2ba58445443f773c0efe4ca2cf76c887e3e9c83f
Robert Posch <posch.robert@gmail.com>
2017-03-15 21:40:04 +0100

    ad files

--- platforms/tests/main_sfml.cpp
+++ platforms/tests/main_sfml.cpp
@@ -2,3 +2,18 @@
 // Created by robert on 15.03.17.
 //
 
+#include <iostream>
+#include <gearboy.h>
+
+int main(int argc, char** argv)
+{
+    GearboyCore gbCore;
+    GB_Color* frameBuffer = new GB_Color[GAMEBOY_WIDTH*GAMEBOY_HEIGHT];
+
+    gbCore.Init();
+    gbCore.LoadROM("roms/Donkey Kong.gb", true);
+
+    gbCore.RunToVBlank(frameBuffer);
+
+    return 0;
+}
