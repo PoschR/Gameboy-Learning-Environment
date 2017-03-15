1c3a5b3b39f99754b818444bb782d9ad8ee069a9
Robert Posch <posch.robert@gmail.com>
2017-03-15 21:55:39 +0100

    complete a sample SFML program

--- platforms/tests/main_sfml.cpp
+++ platforms/tests/main_sfml.cpp
@@ -4,16 +4,62 @@
 
 #include <iostream>
 #include <gearboy.h>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv)
 {
+    sf::RenderWindow window(sf::VideoMode(GAMEBOY_WIDTH,GAMEBOY_HEIGHT), "Gearboy SFML", sf::Style::Close | sf::Style::Titlebar);
+    sf::Texture framebuffer;
+    framebuffer.create(GAMEBOY_WIDTH,GAMEBOY_HEIGHT);
+    sf::Uint8* screen_pixels = new sf::Uint8[GAMEBOY_WIDTH*GAMEBOY_HEIGHT*4];
+
+    window.setFramerateLimit(60);
+
     GearboyCore gbCore;
-    GB_Color* frameBuffer = new GB_Color[GAMEBOY_WIDTH*GAMEBOY_HEIGHT];
+    GB_Color* gbFrameBuffer = new GB_Color[GAMEBOY_WIDTH*GAMEBOY_HEIGHT];
 
     gbCore.Init();
     gbCore.LoadROM("roms/Donkey Kong.gb", true);
+    gbCore.EnableSound(false);
 
-    gbCore.RunToVBlank(frameBuffer);
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        gbCore.RunToVBlank(gbFrameBuffer);
+
+        for(int y=0; y<GAMEBOY_HEIGHT;y++) {
+            for(int x=0; x<GAMEBOY_WIDTH; x++)
+            {
+                int index = y*GAMEBOY_WIDTH+x;
+                int pix_index = 4 * index;
+
+
+                screen_pixels[pix_index+0] = gbFrameBuffer[index].red;
+                screen_pixels[pix_index+1] = gbFrameBuffer[index].green;
+                screen_pixels[pix_index+2] = gbFrameBuffer[index].blue;
+                screen_pixels[pix_index+3] = 255;
+            }
+        }
+        framebuffer.update(screen_pixels);
+
+        sf::Sprite fb_sprite(framebuffer);
+
+
+
+        window.clear(sf::Color::Blue);
+        window.draw(fb_sprite);
+
+        window.display();
+    }
+
+
+
+
 
     return 0;
 }
